3fdc87e02cf65b7f329242a092773ef1f712a59e
lakshmipriya <sajeev.lakshmi@gmail.com>
2021-01-08 09:04:06 +0400

    db connect

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const mongoClient=require('mongodb').MongoClient
 const state={db:null}
 
 module.exports.connect=function(done){
-    const url ='mongodb://localhost:27017'
+    const url ='mongodb://127.0.0.1:27017'
     const dbname='classroom'
     mongoClient.connect(url,{ useUnifiedTopology: true },(err,data)=>{
         if(err)
