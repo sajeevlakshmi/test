51f25993cfa758780583a83964a3734bc5b7e690
lakshmipriya <sajeev.lakshmi@gmail.com>
2021-03-16 07:12:32 +0400

    db

--- config/connection.js
+++ config/connection.js
@@ -2,8 +2,8 @@ const mongoClient=require('mongodb').MongoClient
 const state={db:null}
 
 module.exports.connect=function(done){
-     const url ='mongodb://localhost:27017'
-    // const url='mongodb+srv://eclass:krhrttj5PZ@3.@a@elearning.b3fkm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+     //const url ='mongodb://localhost:27017'
+    const url='mongodb+srv://eclass:krhrttj5PZ@3.@a@elearning.b3fkm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
     const dbname='classroom'
     mongoClient.connect(url,{ useUnifiedTopology: true },(err,data)=>{
         if(err)
